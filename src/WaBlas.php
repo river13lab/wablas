cdcd2ce3410d6e0719b854e3bd5cf28fbec2d9f6
river13lab <river13lab@gmail.com>
2022-11-01 15:18:23 +0700

    fix send bulk
    /api/v2/send-bulk/text  ==> /api/v2/send-message

--- src/WaBlas.php
+++ src/WaBlas.php
@@ -71,7 +71,7 @@ class WaBlas
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
-        curl_setopt($curl, CURLOPT_URL, $this->domain."/api/v2/send-bulk/text");
+        curl_setopt($curl, CURLOPT_URL, $this->domain."/api/v2/send-message");
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
         $result = curl_exec($curl);
